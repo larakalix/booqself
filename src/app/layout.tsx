a2ba7152b7b5f7a6278bbb624754270f4e28f346
Ivan Lara <ukikalix@gmail.com>
2023-05-28 11:53:53 -0400

    Fix metadata from main Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,20 @@ export const metadata: Metadata = {
     title: "Booqself",
     description: "Booqself is an application to make appointments.",
     manifest: "/manifest.json",
+    viewport: "width=device-width, initial-scale=1",
+    openGraph: {
+        type: "website",
+        locale: "en_US",
+        url: "https://booqself.vercel.app",
+        title: "Booqself",
+        description: "Booqself is an application to make appointments.",
+        siteName: "Booqself",
+    },
+    twitter: {
+        title: "Booqself",
+        description: "Booqself is an application to make appointments.",
+        site: "Booqself",
+    },
     icons: {
         icon: "/vercel.svg",
         apple: "/vercel.svg",
