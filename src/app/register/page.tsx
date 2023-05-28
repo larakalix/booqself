dd6d232adbe5177db8ca3da3abf5d8bce80f0df7
Ivan Lara <ukikalix@gmail.com>
2023-05-28 11:19:21 -0400

    Hide formFields render

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,15 +2,16 @@ import { Steps } from "@/components/home/Steps";
 import { PageWrapper } from "@/components/wrapper/PageWrapper";
 import type { IFormField } from "@/types/form";
 
-async function getData() {
-    const res = await fetch(`${process.env.NEXT_API_URL}/form-field`);
-    if (!res.ok) throw new Error("Failed to fetch data");
+// async function getData() {
+//     const res = await fetch(`${process.env.NEXT_API_URL}/form-field`);
+//     if (!res.ok) throw new Error("Failed to fetch data");
 
-    return res.json();
-}
+//     return res.json();
+// }
 
 export default async function Register() {
-    const { formFields } = await getData();
+    // const { formFields } = await getData();
+    const formFields: IFormField[] = [];
 
     if (!formFields) {
         return (
