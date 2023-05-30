043fbfaa5a5b28a90a72d090610cec9492797fce
Ivan Lara <ukikalix@gmail.com>
2023-05-30 16:45:52 -0400

    Clean home component removing client result component and fetch function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,6 @@
-import { Header } from "@/components/home/Header";
-import { Results } from "@/components/home/Clients";
 import { PageWrapper } from "@/components/wrapper/PageWrapper";
-import type { IMeta } from "@/types/strapi/generic";
-import type { IClient } from "@/types/models/clients";
-
-async function getData(): Promise<{ clients: IClient[]; meta: IMeta }> {
-    const res = await fetch(`${process.env.NEXT_API_URL}/client`);
-    if (!res.ok) throw new Error("Failed to fetch data");
-
-    return res.json();
-}
 
 export default async function Home() {
-    const { clients, meta } = await getData();
-
     return (
         <PageWrapper className="flex flex-col gap-8">
             <h1>Hello from Home</h1>
