6dbd36b6012026e1ba2143d0a0d7faabb66f7d3c
Ivan Lara <ukikalix@gmail.com>
2023-05-29 23:56:39 -0400

    Fetch clients data on server component Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,21 @@ import { PageWrapper } from "@/components/wrapper/PageWrapper";
 import type { IMeta } from "@/types/strapi/generic";
 import type { IClient } from "@/types/models/clients";
 
-// async function getData(): Promise<{ clients: IClient[]; meta: IMeta }> {
-//     const res = await fetch(`${process.env.NEXT_API_URL}/client`);
-//     if (!res.ok) throw new Error("Failed to fetch data");
+async function getData(): Promise<{ clients: IClient[]; meta: IMeta }> {
+    const res = await fetch(`${process.env.NEXT_API_URL}/client`);
+    if (!res.ok) throw new Error("Failed to fetch data");
 
-//     return res.json();
-// }
+    return res.json();
+}
 
 export default async function Home() {
-    // const { clients, meta } = await getData();
+    const { clients, meta } = await getData();
 
     return (
         <PageWrapper className="flex flex-col gap-8">
             <Header />
 
-            <Results clients={[]} meta={{} as IMeta} />
+            <Results clients={clients} meta={meta} />
         </PageWrapper>
     );
 }
