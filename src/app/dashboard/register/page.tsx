3136abedf299e9b769ef12778ca0e4037c9a6af7
Ivan Lara <ukikalix@gmail.com>
2023-06-07 13:27:16 -0400

    Remove register view

--- src/app/dashboard/register/page.tsx
+++ src/app/dashboard/register/page.tsx
@@ -1,20 +1,19 @@
-import { Steps } from "@/components/home/Steps";
 import { PageWrapper } from "@/components/wrapper/PageWrapper";
-import type { IFormField } from "@/types/forms/form";
 
-async function getData() {
-    const res = await fetch(`${process.env.NEXT_API_URL}/form-field`);
-    if (!res.ok) throw new Error("Failed to fetch data");
+// async function getData() {
+//     const res = await fetch(`${process.env.NEXT_API_URL}/form-field`);
+//     if (!res.ok) throw new Error("Failed to fetch data");
 
-    return res.json();
-}
+//     return res.json();
+// }
 
 export default async function Register() {
-    const { formFields } = await getData();
+    // const { formFields } = await getData();
 
     return (
         <PageWrapper>
-            <Steps formFields={formFields as IFormField[]} />
+            {/* <Steps formFields={formFields as IFormField[]} /> */}
+            <h1>Register</h1>
         </PageWrapper>
     );
 }
