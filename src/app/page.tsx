d2106d7d56c632cfaecab5c24261ea46109d9338
Ivan Lara <ukikalix@gmail.com>
2023-07-14 02:58:48 -0400

    fx: change main landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,8 @@
-// import { Card } from "@/kit/card/Card";
-// import { Card } from "flowbite-react";
 import { Landing } from "@/components/home/Landing";
 
 export default async function Home() {
     return (
         <section className="min-h-screen w-full p-5">
-            {/* <div className="flex items-center justify-between flex-col gap-4 min-h-[calc(100vh-2.5rem)] p-0"> */}
-            {/* <div className="flex items-center justify-center flex-col gap-4 flex-1">
-                    <h1 className="text-main-blue text-[1.5rem] font-semibold">
-                        Pura Vida
-                    </h1>
-                    <Landing />
-                </div>
-                <footer className="py-6">
-                    <p className="text-gray-400 text-sm">
-                        Copyright © 2023 Pura Vida. All rights reserved.
-                    </p>
-                </footer> */}
-
             <div className="flex items-center justify-center gap-4 text-center bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700 min-h-[calc(100vh-2.5rem)]">
                 <section className="bg-white dark:bg-gray-900">
                     <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
