892c3ce5f21e00dfe255f396f89f7d1fb7b23d28
Ivan Lara <ukikalix@gmail.com>
2023-07-15 17:29:44 -0400

    ft: read appointment form type from endpoint and apply into the factory component in order to render dynamically the respective format

--- src/components/book/index.ts
+++ src/components/book/index.ts
@@ -1,4 +1,4 @@
-export { Apointments } from "./appointments/Appointments";
+export { Appointments } from "./appointments/Appointments";
 export { Calendar } from "./Calendar";
 export { Day } from "./Day";
 export { AppointmentForm } from "./scheduleTypes/types/AppointmentForm";
